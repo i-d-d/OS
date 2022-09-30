48c38b36244a3f254f36769c83b9f9da933e9055
IDDQD <maxseimov.idd@gmail.com>
2022-09-30 13:42:49 +0300

    Done

--- os_lab_1/src/child.c
+++ os_lab_1/src/child.c
@@ -2,21 +2,48 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define STR_LEN 32
+
+int read_float(int fd, float *f) {
+    char *buf = calloc(sizeof(char), STR_LEN);
+    char c = 0;
+    short i = 0;
+    if (read(fd, &c, 1) < 0) {
+        exit(3);
+    }
+    if ((c == ' ') || (c == '\n') || (c == '\0'))
+        return 0;
+    while (c != ' ' && c != '\n') {
+        buf[i++] = c;
+        read(fd, &c, 1);
+    }
+
+
+    *f = strtof(buf, NULL);
+    return 1;
+}
 
 int main() {
     float init_num = 0, div_num = 0;
 
-    scanf("%f", &init_num);
+    if (read_float(0, &init_num) < 1) {
+        exit(4);
+    }
 
-    while (scanf("%f", &div_num) != EOF) {
+    while (read_float(0, &div_num) != 0) {
         
         if (div_num == 0) {
-            exit(EXIT_FAILURE);
+            exit(1);
         }
 
         init_num /= div_num;
     }
     
-    write(1, &init_num, sizeof(init_num));
+    if (write(1, &init_num, sizeof(init_num)) != sizeof(init_num)) {
+        exit(2);
+    }
+
     return 0;
 }
--- os_lab_1/src/main.c
+++ os_lab_1/src/main.c
@@ -6,22 +6,28 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <err.h>
 
 #define STR_LEN 128
 
+char *const child_args[] = { "child", NULL };
+char *child_env[] = { NULL };
+
+size_t strlen_new(char *s) {
+    for (size_t i = 0; i < strlen(s); ++i) {
+        if (s[i] == '\n')
+            return i;
+    }
+    return strlen(s);
+}
 
 int main() {
 
-    char *const child_args[] = { "child", NULL };
-    char *child_env[] = { NULL };
-
-    char fname_buf[STR_LEN];
-    memset(fname_buf, '\0', STR_LEN);
+    char *fname_buf = calloc(sizeof(char), STR_LEN);
     read(0, fname_buf, STR_LEN);
 
-    char *fname = malloc(sizeof(char) * strlen(fname_buf));
-    strncpy(fname, fname_buf, strlen(fname_buf));
+    char *fname = malloc(sizeof(char) * strlen_new(fname_buf));
+    strncpy(fname, fname_buf, strlen_new(fname_buf));
+    free(fname_buf);
 
     pid_t fd[2];
     pipe(fd);
@@ -34,8 +40,8 @@ int main() {
 
     pid_t pid = fork();
 
-    if (pid == -1) {
-        perror("fork() error");
+    if (pid < 0) {
+        perror("fork");
         exit(EXIT_FAILURE);
     }
 
@@ -51,33 +57,54 @@ int main() {
             exit(EXIT_FAILURE);
         }
         execve(child_args[0], child_args, child_env);
-        perror("execve()");
+        perror("execve");
     }
 
     else {              // Parent
 
         int child_exit_status;
-        waitpid(pid, &child_exit_status, 0);
-        if (!WIFEXITED(child_exit_status)) {
-            perror("Child process error exit");
+        if (waitpid(pid, &child_exit_status, 0) < 0) {
+            perror("waitpid");
+        }
+        if (WEXITSTATUS(child_exit_status) == 1) {
+            char *err1 = "Child process error exit, divison by zero\n";
+            write(2, err1, strlen(err1));
+            exit(EXIT_FAILURE);
+        }
+
+        if (WEXITSTATUS(child_exit_status) == 4) {
+            char *err2 = "Child process error exit, expected float number\n";
+            write(2, err2, strlen(err2));
+            exit(EXIT_FAILURE);
+        }
+
+        else if (WEXITSTATUS(child_exit_status) != 0) {
+            perror("Child process error");
             exit(EXIT_FAILURE);
         }
 
         float result;
-        if (read(fd[0], &result, sizeof(result)) != sizeof(result)) {
-            err(EXIT_FAILURE, "Can't read from pipe");
+        if (read(fd[0], &result, sizeof(result)) < 0) {
+            perror("Can't read from pipe");
             exit(EXIT_FAILURE);
         }
-        
-        /* if (write(1, &result, sizeof(result)) != sizeof(result)) {
-            err(EXIT_FAILURE, "Can't write into pipe");
-            exit(EXIT_FAILURE);
-        } */
 
         close(fd[0]);
         close(fd[1]);
+        close(filedes);
 
-        printf("\n%f\n", result);
+        char *result_string = calloc(sizeof(char), STR_LEN);
+        gcvt(result, 7, result_string);
+        if (write(1, result_string, strlen(result_string)) < 0) {
+            perror("Can't write result to 1");
+            exit(EXIT_FAILURE);
+        }
+
+        char endline_c = '\n';
+        if (write(1, &endline_c, 1) < 0) {
+            perror("Can't write \\n to 1");
+            exit(EXIT_FAILURE);
+        }
     }
 
     return 0;
